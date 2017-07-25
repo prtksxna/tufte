8891bc7b123ab7172aaaf7c11a7b2b1d2fd0988f
Prateek Saxena <prtksxna@gmail.com>
2017-07-25 16:55:19 +0530

    feat: add webfonts

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
+	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,600,600i|Lato:400,700" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
