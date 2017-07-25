69068cae8bda32112cf514d70cbea1e20d36e619
Prateek Saxena <prtksxna@gmail.com>
2017-07-25 18:57:46 +0530

    fix: layout and post footer

--- template-parts/content.php
+++ template-parts/content.php
@@ -16,14 +16,7 @@
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php tufte_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		endif;?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -49,6 +42,14 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		<?php
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php tufte_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+
 		<?php tufte_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
